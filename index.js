676e7b032c741efcddec270e110f9a3beb040359
DESKTOP-RF047AL\wooooooak <wooooooak@gmail.com>
2018-07-07 22:29:15 +0900

    configuration typecsript

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = 'wooooooak', age = 24, gender = 'male';
+const sayHi = (name, age) => {
+    console.log(`hello ${name}`);
+};
+sayHi('wooooooak', 111);
+//# sourceMappingURL=index.js.map
